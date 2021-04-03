22dce70286cde31c92e2eb369497ea87369549e7
alegzz <bo@alegzz.ru>
2021-04-03 17:48:51 +0300

    add requirements.txt, change variables name, change to systemd and re2 libs

--- cctaxi.py
+++ cctaxi.py
@@ -4,50 +4,39 @@ from telethon.tl.types import PeerUser, PeerChat, PeerChannel
 from telethon import functions, types
 from telethon.tl.functions.channels import JoinChannelRequest
 
-import re
+import re2 as re
 
 import asyncio
-import systemd.daemon
+from systemd.daemon import notify, Notification
 
 from config import settings
 
+client = TelegramClient(settings.session, settings.api_id, settings.api_hash)
+client.start()
+
 pattern = re.compile(settings.pattern)
 
-client = TelegramClient(settings.session, settings.api_id, settings.api_hash)
-
-loop = asyncio.get_event_loop()
-
-async def main():
-    systemd.daemon.notify('READY=1')
-    await client.get_dialogs()
-
-    global taxichan
-    taxichan = await client.get_entity(settings.channel)
-
 async def doit(event, is_album):
-    if is_album:
-        to_id = event.messages[0].to_dict()['peer_id']
-    else:
-        to_id = event.message.to_dict()['peer_id']
+    to_id = event.messages[0].to_dict()['peer_id'] if is_album else event.message.to_dict()['peer_id']
 
-    if to_id['_'] == 'PeerUser':
+    to_peer = to_id['_']
+
+    if to_peer == 'PeerUser':
         if to_id['user_id'] in settings.admins:
-            if is_album:
-                chan_id = event.messages[0].fwd_from.to_dict()['from_id']
-            else:
-                chan_id = event.message.fwd_from.to_dict()['from_id']
+            chan_id = event.messages[0].fwd_from.to_dict()['from_id'] if is_album else event.message.fwd_from.to_dict()['from_id']
             if chan_id['_'] == 'PeerChannel':
                 channel = await client.get_entity(chan_id['channel_id'])
                 await client(JoinChannelRequest(channel))     
 
-    if to_id['_'] != 'PeerChannel':
+    if to_peer != 'PeerChannel':
         return
 
-    if to_id['channel_id'] == taxichan.id:
+    if to_id['channel_id'] == fwd_channel.id:
         return
 
-    if pattern.findall(event.raw_text.lower()) != []:
-         await event.forward_to(taxichan)
+    if (to_peer == 'PeerChannel' and
+        pattern.findall(event.raw_text.lower())):
+         await event.forward_to(fwd_channel)
 
 @client.on(events.Album)
 async def handler(event):
@@ -59,8 +48,16 @@ async def handler(event):
     if event.message.to_dict()['grouped_id'] == None:
         await doit(event, False)
     return
-   
-client.start()
+
+async def main():
+    notify(Notification.READY)
+    await client.get_dialogs()
+    global fwd_channel
+    fwd_channel = await client.get_entity(settings.forward_channel)
+
+    while client.is_connected():
+        notify(Notification.STATUS, "I'm fine")
+        await asyncio.sleep(10)
 
 client.loop.run_until_complete(main())
-client.run_until_disconnected()
+#client.run_until_disconnected()
