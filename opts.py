10907039e81d4fe8cd79321853bd1235e439de7f
alegzz <bo@alegzz.ru>
2021-04-17 19:11:50 +0300

    sessions, check if running from systemd

--- opts.py
+++ opts.py
@@ -10,6 +10,7 @@ class Options:
 
         (options, args) = parser.parse_args()
 
-        self.configname = path.abspath(options.configname)
+        self.configname = path.abspath(options.configname) if options.configname else options.configname
+
         if not self.configname:
             self.configname = configname
--- tgwatch.py
+++ tgwatch.py
@@ -4,24 +4,21 @@ from telethon.tl.types import PeerUser, PeerChat, PeerChannel
 from telethon import functions, types
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.sessions import StringSession
+
 import re2 as re
 import asyncio
+
 from systemd.daemon import notify, Notification
+
+from psutil import Process
+from os import getpid
+
 from config import Settings
 from sessions import Session
 from opts import Options
 
-def isadmin(PeerId, admins):
-    result = False
-    for a in admins:
-        if PeerId == admins[a]['id']:
-            result = True
-            break
-    return result
-
 options = Options()
 settings = Settings(options.configname)
-print(settings.admins)
 session = Session(settings.sessionfile)
 client = TelegramClient(StringSession(session.string), settings.api_id, settings.api_hash)
 client.start()
@@ -29,7 +26,11 @@ string = StringSession.save(client.session)
 if string != session.string:
     session.save(string)
 
+is_systemd = Process(getpid()).ppid() == 1
+
 pattern = re.compile(settings.pattern, re.IGNORECASE)
+
+admins = [settings.admins[admin]['id'] for admin in settings.admins]
 
 async def doit(event, is_album):
     to_id = event.messages[0].to_dict()['peer_id'] if is_album else event.message.to_dict()['peer_id']
@@ -37,41 +38,39 @@ async def doit(event, is_album):
     to_peer = to_id['_']
 
     if to_peer == 'PeerUser':
-        if isadmin(to_id['user_id'], settings.admins):
+        if to_id['user_id'] in admins:
             chan_id = event.messages[0].fwd_from.to_dict()['from_id'] if is_album else event.message.fwd_from.to_dict()['from_id']
             if chan_id['_'] == 'PeerChannel':
                 channel = await client.get_entity(chan_id['channel_id'])
                 await client(JoinChannelRequest(channel))     
 
-    if to_peer != 'PeerChannel':
+    if to_peer != 'PeerChannel' or to_id['channel_id'] == fwd_channel.id:
         return
 
-    if to_id['channel_id'] == fwd_channel.id:
-        return
-
-    if (to_peer == 'PeerChannel' and
-        pattern.search(event.raw_text)):
+    if pattern.search(event.raw_text):
          await event.forward_to(fwd_channel)
 
 @client.on(events.Album)
 async def handler(event):
     await doit(event, True)
-    return
 
 @client.on(events.NewMessage)
 async def handler(event):
     if event.message.to_dict()['grouped_id'] == None:
         await doit(event, False)
-    return
 
 async def main():
-    notify(Notification.READY)
+    if is_systemd:
+        notify(Notification.READY)
     await client.get_dialogs()
     global fwd_channel
     fwd_channel = await client.get_entity(settings.forward_channel)
 
     while client.is_connected():
-        notify(Notification.STATUS, "I'm fine")
-        await asyncio.sleep(10)
+        if is_systemd:
+            notify(Notification.STATUS, "I'm fine")
+            await asyncio.sleep(10)
+        else:
+            await asyncio.sleep(3600)
 
 client.loop.run_until_complete(main())
